e13b7f1ebbb7b2dc405bde98d09d4d51ae8fe9e2
sion <sion.andreu@gmail.com>
2021-01-17 19:07:52 +0100

    Pujada inicial

---
+++ src/main/java/es/caib/notib/permis/NotibmsPermisServiceApplication.java
@@ -0,0 +1,13 @@
+package es.caib.notib.permis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotibmsPermisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotibmsPermisServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/caib/notib/permis/NotibmsPermisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.caib.notib.permis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotibmsPermisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
